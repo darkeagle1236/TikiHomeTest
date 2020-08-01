c365c9d155b98b9172b273a321b996c958e0e691
darkeagle1236 <phonglythanh14@gmail.com>
2020-08-02 01:21:41 +0700

    Delete unnecessary implementation

--- app/src/main/java/com/example/tikihometest/banner/BannerPresenterImpl.kt
+++ app/src/main/java/com/example/tikihometest/banner/BannerPresenterImpl.kt
@@ -3,19 +3,10 @@ package com.example.tikihometest.banner
 import com.example.tikihometest.model.banner.BannerResponse
 import retrofit2.Response
 
-class BannerPresenterImpl(var iView:BannerContract.BannerView):BannerContract.BannerPresenter,OnGetBannerListFinishListener {
+class BannerPresenterImpl(var iView:BannerContract.BannerView):BannerContract.BannerPresenter {
     var iModel = BannerModel()
     override suspend fun getBanner():Response<BannerResponse> {
         iView.showBannerProgress()
         return iModel.callGetBannerApi()
-    }
-
-    override fun onSuccess(response: BannerResponse) {
-        iView.showBannerResult(response)
-        iView.hideBannerProgress()
-    }
-
-    override fun onFailure() {
-        iView.hideBannerProgress()
     }
 }
